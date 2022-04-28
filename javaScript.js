02143ee991f4e49506ad706d6159f2d6416fbfa0
jahseed89 <samson.ocran@gmail.com>
2022-04-28 16:16:19 +0100

    date updated

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,9 @@
+const now = new Date()
+
 
 const curentDate = document.getElementById("empty");
-curentDate.innerHTML = "Date: " + Date();
-// curentDate.style.color = "blue";
-const infoLink = document.getElementById('tribute-link');
-// infoLink.style.fontSize = "20px";
+
+curentDate.innerHTML = "Date: " + now;
+
+// const infoLink = document.getElementById('tribute-link');
+
